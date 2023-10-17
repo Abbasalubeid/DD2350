7fcb87824facb6dd449c2937333f495a4c3d7673
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-10-17 08:40:46 +0200

    optimal numbering using exhaustive search and heaps algorithm

--- Graphs/CheapNumbering.java
+++ Graphs/CheapNumbering.java
@@ -48,4 +48,117 @@ public class CheapNumbering {
         }
     }
 
+    public static boolean edgeExists(Graph g, int vertex1, int vertex2) {
+        for (Graph.Edge edge : g.edges) {
+            if ((edge.u == vertex1 && edge.v == vertex2) || (edge.u == vertex2 && edge.v == vertex1)) {
+                return true;
+            }
+        }
+        return false;
+    }    
+
+    public Map<Integer, Integer> cheapNumbering(Graph g) {
+        List<Integer> vertices = g.vertices;
+        List<Graph.Edge> edges = g.edges;
+        Map<Integer, Integer> optimalAssignment = new HashMap<>();
+        List<Integer> optimalNumbering = new ArrayList<>();
+        int minSum = Integer.MAX_VALUE;
+
+        List<List<Integer>> allPermutations = new ArrayList<>();
+        generatePermutations(new ArrayList<>(vertices), vertices.size(), allPermutations);
+
+        
+        for (List<Integer> currentPermutation : allPermutations) {
+            int currentSum = 0;
+            System.out.println("\nChecking permutation: " + currentPermutation);
+        
+            for (Graph.Edge edge : edges) {
+                int difference = Math.abs(currentPermutation.get(edge.u - 1) - currentPermutation.get(edge.v - 1));
+                System.out.println("Edge (" + edge.u + ", " + edge.v + ") has difference " + difference);
+                currentSum += difference;
+            }
+        
+            System.out.println("Total difference for this permutation: " + currentSum);
+        
+            if (currentSum < minSum) {
+                System.out.println("This is the best permutation found so far!");
+                minSum = currentSum;
+                minimumDifference = minSum;
+                optimalNumbering = new ArrayList<>(currentPermutation);
+            }
+            else if (currentSum == minSum){
+                System.out.println("This is also one possible solution!");
+            }
+        }
+        
+
+        for (int i = 0; i < optimalNumbering.size(); i++) {
+            optimalAssignment.put(vertices.get(i), optimalNumbering.get(i));
+        }
+
+        return optimalAssignment;
+    }
+
+
+    //Heap’s Algorithm for generating permutations
+    // Time Complexity: O(N*N!), where N is the size of the given array 'a'
+    private void generatePermutations(List<Integer> a, int size, List<List<Integer>> allPermutations) {
+        if (size == 1) {
+            allPermutations.add(new ArrayList<>(a));
+            return;
+        }
+
+        for (int i = 0; i < size; i++) {
+            generatePermutations(a, size - 1, allPermutations);
+
+
+            if (size % 2 == 1) {
+                Collections.swap(a, 0, size - 1);
+            } else {
+                Collections.swap(a, i, size - 1);
+            }
+        }
+    }
+
+// ... (rest of the code below remains unchanged)
+
+    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int optimalDiffs = 0;
+        
+        // Get user input for the number of vertices
+        System.out.print("Enter the number of vertices: ");
+        int numVertices = Integer.parseInt(scanner.nextLine());  // Using nextLine here to prevent issues with lingering newline characters
+    
+        Graph g = new Graph(numVertices);
+        
+        for (int i = 1; i <= numVertices; i++) {
+            System.out.print("Enter vertices adjacent to vertex " + i + " (separated by space, press Enter when done): ");
+            String[] adjacentVerticesStr = scanner.nextLine().split(" ");  // Splitting the line input into individual vertices
+    
+            for (String vStr : adjacentVerticesStr) {
+                if (!vStr.trim().isEmpty()) {  // Check to ensure string is not empty
+                    int v = Integer.parseInt(vStr);
+                    if (v != i && v <= numVertices && v >= 1 && !edgeExists(g, i, v)) {
+                        g.addEdge(i, v);
+                    }
+                }
+            }
+        }
+    
+        g.visualize();
+    
+        CheapNumbering solver = new CheapNumbering();
+        System.out.println("Starting the exhaustive search...\n");
+        Map<Integer, Integer> result = solver.cheapNumbering(g);
+        System.out.println("\nExhaustive search completed!");
+    
+        System.out.println("\nOptimal Numbering: (total difference: " + solver.minimumDifference   + ")");
+        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+            System.out.println("Vertex " + entry.getKey() + " is assigned number " + entry.getValue());
+        }
+    }
 }
+
